cd6507c8c185d09ca5ba3136c27390c8dcc5f5e4
SpencerTower <spencerjtower1@gmail.com>
2021-12-13 16:52:54 -0800

    adds props to BooksCarousel.js

--- src/BooksCarousel.js
+++ src/BooksCarousel.js
@@ -8,41 +8,17 @@ class BooksCarousel extends Component {
     return (
       <div>
         <Carousel fade>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=First slide&bg=373940"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
-              alt="Second slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
-              alt="Third slide"
-            />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
+            {this.props.books.map(book => {
+                return (
+                    <Carousel.Item>
+                    <Carousel.Caption>
+                    <h3>{book.title}</h3>
+                    <p>{book.description}</p>
+                    <p>{book.status}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                )
+            })}
         </Carousel>
       </div>
     );
@@ -50,3 +26,4 @@ class BooksCarousel extends Component {
 }
 
 export default BooksCarousel;
+
