4580d2456316bdaa24afec2bc356fd73b32b7f03
SpencerTower <spencerjtower1@gmail.com>
2021-12-15 17:03:48 -0800

    adds new carousel image, adds card to carousel

--- src/BooksCarousel.js
+++ src/BooksCarousel.js
@@ -1,5 +1,9 @@
 import Carousel from 'react-bootstrap/Carousel';
 import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import { CarouselItem } from 'react-bootstrap';
 
 
 class BooksCarousel extends Component {
@@ -7,25 +11,23 @@ class BooksCarousel extends Component {
   render() {
     return (
       <div>
-        <Carousel fade>
-          {this.props.books.map(book => {
-            return (
-              <Carousel.Item style={{ color: 'blue' }} key={book._id}>
-                <img style={{ width: '200px', height: '800px' }}
-                  className="d-block w-100"
-                  src="https://libribook.com/Images/the-windup-universe-01-windup-girl-pdf.jpg"
-                  alt="First slide"
-                />
-                <Carousel.Caption >
-                  <h3>{book.title}</h3>
-                  <p>{book.description}</p>
-                  <p>Status: {book.status}</p>
-                </Carousel.Caption>
-              </Carousel.Item>
-            )
-          })}
-        </Carousel>
-      </div>
+        <Card style={{ width: '40rem', height: '25rem' }}>
+          <Carousel variant="dark" fade>
+            {this.props.books.map(book => {
+              return (
+                <Carousel.Item key={book._id}>
+                  <Image src="https://cdn.shopify.com/s/files/1/2329/3055/articles/ReadingToUnderstandOurWorld_2048x.jpg?v=1521864290" alt="First slide" rounded fluid />
+                  <Carousel.Caption style={{ top: 55 }}>
+                    <h3 style={{ height: '1.2em'}}>{book.title}</h3>
+                    <p>{book.description} </p>
+                    <p><b>Status: {book.status}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              )
+            })}
+          </Carousel>
+        </Card>
+      </div >
     );
   }
 }
